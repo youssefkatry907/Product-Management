ed33c82fb4998c4e48b64b08ce84d2bf6d5fe6c7
youssef Qatry <74834762+youssefkatry907@users.noreply.github.com>
2025-02-01 23:01:43 +0200

    Update auth-response.dto.ts

--- src/auth/dtos/auth-response.dto.ts
+++ src/auth/dtos/auth-response.dto.ts
@@ -20,7 +20,7 @@ export class AuthResponseDto {
       id: '5d725a037b292f5f8ceff789',
       email: "example@gmail,com",
       role: "user",
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkNzI1YTAzN2IyOTJmNWY4Y2VmZjc4OSIsImlhdCI6MTU2NjIwMjM1OSwiZXhwIjoxNTY2Mjg4NzU5fQ.1e3Gj5e6vzQz7rL0QD5vFjw6Y6J7vX8uQ6L8ZP7GJ7g"
+      token: "token"
     },
     description: 'Response data'
   })
